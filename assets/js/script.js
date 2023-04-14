e318802b7ba1c1f368a1e41856f92400e78fa315
simonepietraroia <simonepietraroia21@gmail.com>
2023-04-14 18:06:40 +0000

    fixed html so that the javascript would properlyrepond to the element with question id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,66 @@
 // This is going to contain all the questions //
 const questions = [
     {
-        question: "What is the capital of Italy?";,
-        options: ["Rome", "Paris", "Madrid", "London"];,
-        answer: 0;
+        question: "What is the capital of Italy?",
+        options: ["Rome", "Paris", "Madrid", "London"],
+        answer: 0
     },
 
     {
-        question: "What is the country with the largest population in Europe?";,
-        options: ["Germany", "Russia", "England", "Poland"];,
-        answer: 1;
+        question: "What is the country with the largest population in Europe?",
+        options: ["Germany", "Russia", "England", "Poland"],
+        answer: 1
     },
 
     {
-        question: "How many states does the United States have?";,
-        options: ["52", "49", "48", "50"];,
-        answer: 3;
+        question: "How many states does the United States have?",
+        options: ["52", "49", "48", "50"],
+        answer: 3
     },
 
     {
-        question: "What is the capital of Japan?";,
-        options: ["Bejing", "Tokyo", "Moscow", "Manila"];,
-        answer: 1;
+        question: "What is the capital of Japan?",
+        options: ["Bejing", "Tokyo", "Moscow", "Manila"],
+        answer: 1
     },
 
     {
-        question: "Select the country with the largest territory";,
-        options: ["Canada", "South Africa", "Russia", "Brazil"];,
-        answer: 2;
+        question: "Select the country with the largest territory?",
+        options: ["Canada", "South Africa", "Russia", "Brazil"],
+        answer: 2
     },
 
     {
-        question: "What country has the largest border with a french territory";,
-        options: ["Brazil", "Monaco", "Belgium", "Switzerland"]
-        answer: 0;
+        question: "What country has the largest border with a french territory?",
+        options: ["Brazil", "Monaco", "Belgium", "Switzerland"],
+        answer: 0
     },
 
     {
-        question: "Who is the current prime minister of England?";,
-        options: ["Rishi Sunak", "Boris Johnson", "Liz Truss", "Humza Yousaf"];,
-        answer: 0;
+        question: "Who is the current prime minister of England?",
+        options: ["Rishi Sunak", "Boris Johnson", "Liz Truss", "Humza Yousaf"],
+        answer: 0
     },
 
     {
-        question: "What country invented Ice Cream?";,
-        options: ["Italy", "United States", "Ethiopia", "China"];,
-        answer: 3;
+        question: "What country invented Ice Cream?",
+        options: ["Italy", "United States", "Ethiopia", "China"],
+        answer: 3
     },
 
     {
-        question: "What is the longest non-stop commercial flight?";,
-        options: ["Perth to London", " Houston to Sydney", "Singapore to New York", " Dubai to Auckland"];,
-        answer: 2;
+        question: "What is the longest non-stop commercial flight?",
+        options: ["Perth to London", " Houston to Sydney", "Singapore to New York", " Dubai to Auckland"],
+        answer: 2
     }
 ]
 
 let currentQuestion = 0;
 let score = 0;
 
-const questionElem = document.getElementById("question-display");
+const questionElem = document.getElementById("question");
 const optionElems = document.querySelectorAll("#options button");
-const scoreElem = document.getElementById("score");
+const scoreElem = document.getElementById("score")
 
 /**
  * This function goes through the questions listed in the questions variable and cycles through them.
@@ -75,8 +75,24 @@ function displayQuestion() {
 	}
 }
 
-function checkAnswer() {
-
+/**
+ * This function checks the answer after reciving the users choice rom the displayQuestion function.
+ * It also increases the score after every question if correct
+ * it also determins whether theere are any more questions
+ */
+function checkAnswer(event) {
+    let selectedOption = event.target;
+	let question = questions[currentQuestion];
+	if (question.answer === parseInt(selectedOption.id.slice(-1))) {
+		score++;
+		scoreElem.innerText = score;
+	}
+	currentQuestion++;
+	if (currentQuestion === questions.length) {
+		alert(" Congradulations! Your score is " + score);
+	} else {
+		displayQuestion();
+	}
 }
 
 displayQuestion();
